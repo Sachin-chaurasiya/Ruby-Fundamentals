205ff333592d6dd735edec7b82a0fa579575fc52
Sachin-chaurasiya <sachinchaurasiya.dev@gmail.com>
2025-01-26 18:09:07 +0530

    Add logical error handling and discount calculation methods

--- mod3/logical_error.rb
+++ mod3/logical_error.rb
@@ -11,3 +11,21 @@ begin
 rescue ZeroDivisionError => e
   puts "Specific Error occurred: #{e.message}"
 end
+
+def calculate_discount(items, amount)
+  # here we are using logical OR operator instead of logical AND operator
+  # so the condition will always be true and the first block will always be executed
+  if items > 5 || amount > 100
+    'You are eligible for a discount'
+  else
+    'You are not eligible for a discount'
+  end
+end
+
+def safe_division(dividend, divisor)
+  return 0 if divisor.zero?
+
+  dividend / divisor
+end
+
+safe_division(10, 0)
