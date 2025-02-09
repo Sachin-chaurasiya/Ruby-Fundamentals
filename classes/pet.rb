9541666cf1af4f03146fa107f0b073ccb74502fe
Sachin-chaurasiya <sachinchaurasiya.dev@gmail.com>
2025-02-09 18:15:53 +0530

    Add Pet class to manage pet information and list all pets

---
+++ classes/pet.rb
@@ -0,0 +1,37 @@
+class Pet
+  # accessor allow for both read and write the instance variables
+  attr_accessor :name
+  # reader allow for read the instance variables
+  attr_reader :species
+  # writer allow for write the instance variables
+  attr_writer :species
+
+  # class variable and it can't be accessed from outside the class
+  @@all_pets = []
+
+  def initialize(name, species)
+    @name = name
+    @species = species
+
+    # add the new pet to the class variable
+    @@all_pets << self
+  end
+
+  def display_info
+    puts "Name: #{@name}"
+    puts "Species: #{@species}"
+  end
+
+  # class method
+  def self.list_all_pets
+    @@all_pets.each do |pet|
+      puts "Name: #{pet.name}"
+      puts "Species: #{pet.species}"
+    end
+  end
+
+  # virtual attribute
+  def is_cat?
+    @species == 'Cat'
+  end
+end
